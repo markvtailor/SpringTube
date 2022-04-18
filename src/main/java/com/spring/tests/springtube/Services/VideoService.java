aaa7704541126aa1867482331abd5e27ec1089f8
Mark Tailor <markvtailor@gmail.com>
2022-04-18 20:22:19 +0300

    Docker

--- src/main/java/com/spring/tests/springtube/Services/VideoService.java
+++ src/main/java/com/spring/tests/springtube/Services/VideoService.java
@@ -51,7 +51,7 @@ public class VideoService {
     AwsBasicCredentials awsCreds = AwsBasicCredentials.create("123","xyz");            
 
     public VideoEntity uploading(String name, String description, MultipartFile file) throws IOException, URISyntaxException {
-        final S3Client s3 = S3Client.builder().endpointOverride(new URI("http://localhost:4566")).credentialsProvider(EnvironmentVariableCredentialsProvider.create()).region(Region.EU_NORTH_1).build();
+        final S3Client s3 = S3Client.builder().endpointOverride(new URI("http://localstack:4566")).credentialsProvider(EnvironmentVariableCredentialsProvider.create()).region(Region.EU_NORTH_1).build();
         ListBucketsRequest listBucketsRequest = ListBucketsRequest.builder().build();
         ListBucketsResponse listBucketsResponse = s3.listBuckets(listBucketsRequest);
         if(listBucketsResponse.buckets().isEmpty()){
@@ -79,14 +79,14 @@ public class VideoService {
     }
 
     public void deleting(String UUID) throws URISyntaxException {
-        final S3Client s3 = S3Client.builder().endpointOverride(new URI("http://localhost:4566")).region(Region.EU_NORTH_1).build();
+        final S3Client s3 = S3Client.builder().endpointOverride(new URI("http://localstack:4566")).region(Region.EU_NORTH_1).build();
         DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder().bucket("bucket1").key(UUID).build();
         s3.deleteObject(deleteObjectRequest);
         videoRepository.deleteByUniqueVideoId( UUID);
     }
 
     public ResponseEntity<byte[]> streaming(String UUID) throws URISyntaxException  {
-        final S3Client s3 = S3Client.builder().endpointOverride(new URI("http://localhost:4566")).region(Region.EU_NORTH_1).build();
+        final S3Client s3 = S3Client.builder().endpointOverride(new URI("http://localstack:4566")).region(Region.EU_NORTH_1).build();
         try{
             GetObjectRequest getObjectRequest = GetObjectRequest.builder().bucket("bucket1").key(UUID).build();
             ResponseBytes<GetObjectResponse> objectBytes = s3.getObjectAsBytes(getObjectRequest);
